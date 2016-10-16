a41b0098a831389d2dd608795694e9a2e68e4341
Kojin Oshiba <kojin.oshiba@gmail.com>
2016-10-16 02:22:29 -0700

    page scroll

---
+++ js/nn.js
@@ -0,0 +1,29 @@
+jsPlumb.ready(function() {
+    var common = {
+        connector: ["Straight"],
+        anchor: ["Left", "Right"],
+        endpoint:"Blank"
+    };
+    for (var i = 1; i < 3; i++) {
+       for (var j = 1; j < 4; j++) {
+         jsPlumb.connect({
+             source:"f-" + i,
+             target:"t-" + j,
+             paintStyle:{ strokeStyle:"lightgray", lineWidth:3 },
+             endpointStyle:{ fillStyle:"lightgray", outlineColor:"gray" },
+             overlays:[
+                 ["Arrow" , { width:12, length:12, location:0.67 }]
+             ]
+         }, common);
+         jsPlumb.connect({
+             source:"t-" + j,
+             target:"w-" + i,
+             paintStyle:{ strokeStyle:"lightgray", lineWidth:3 },
+             endpointStyle:{ fillStyle:"lightgray", outlineColor:"gray" },
+             overlays:[
+                 ["Arrow" , { width:12, length:12, location:0.67 }]
+             ]
+         }, common);
+       }
+    }
+});
